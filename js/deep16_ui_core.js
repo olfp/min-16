3d35cede77eab6e5f1b853c3b4881c8795379558
Olaf Püschel <olf@unixpro.de>
2025-11-24 12:15:11 +0100

    Update deep16_ui_core.js

--- js/deep16_ui_core.js
+++ js/deep16_ui_core.js
@@ -2,8 +2,8 @@
 class DeepWebUI {
     constructor() {
         this.assembler = new Deep16Assembler();
-        this.disassembler = new Deep16Disassembler();
         this.simulator = new Deep16Simulator();
+        this.disassembler = new Deep16Disassembler();
         this.simulator.setUI(this);
         
         this.memoryStartAddress = 0;
